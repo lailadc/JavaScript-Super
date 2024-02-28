0ace5c970ad5ae4910e776d67c67cbcc059cb086
Rebeca Laila <rebeca.ldcosta@gmail.com>
2024-02-28 10:11:31 -0300

    Praticando JavaScript
    Progredindo um pouco mais em um código ainda inacabado.

--- Aula05/JSSprova05.js
+++ Aula05/JSSprova05.js
@@ -8,9 +8,9 @@ diárias, garantindo que tudo seja feito de forma organizada.
 
 const OPCOES = "1  -  Adicionar tarefa\n" +
     "2  -  Remover tarefa\n3  -  Atualizar tarefa\n0  -  Encerrar programa\n";
-let opcao, membro, mensagem, atividade, i;
+let opcao, membro, mensagem, atividade, visualizar, espacos, i, j, k;
 let sair = false
-let membros = ["pai", "mãe", "irmã"];
+let membros = ["PAI", "MÃE", "IRMÃ"];
 let tarefas = [
     ["trabalhar"],
     ["treinar", "trabalhar"],
@@ -18,14 +18,45 @@ let tarefas = [
 ];
 
 do {
+    visualizar = "---------   M  E  M  B  R  O  S   ---------\n";
+    for (pessoa of membros) {
+        espacos = '';
+        for (i = 0; i < (15 - pessoa.length)/2; i++) {
+            espacos += ' ';
+        }
+        visualizar += `${espacos}${pessoa}${espacos}`;
+    }
+    j = 0;
+    for (letra of "TAREFAS") {
+        visualizar += `\n${letra}`;
+        for (i = 0; i < membros.length; i++) {
+            espacos = '';
+            if (j < tarefas[i].length) {
+                for (k = 0; k < (15 - tarefas[i][j].length)/2; k++) {
+                    espacos += ' ';
+                }
+                visualizar += `${espacos}${tarefas[i][j]}${espacos}`;
+            } else {
+                for (k = 0; k < 15; k++) {
+                    espacos += ' ';
+                }
+                visualizar += espacos;
+            }
+        }
+        visualizar += '\n';
+        j++;
+    }
+    console.log(visualizar);
+    alert(visualizar);
+
     opcao = prompt(OPCOES + "Informe o número correspondente à opção desejada:");
     switch (opcao) {
-        case '1':
+        case '1':  // ADICIONAR TAREFA
             atividade = prompt("Informe a atividade a ser adicionada:").toUpperCase();
 
             // ESCOLHENDO MEMBRO DA FAMÍLIA
             mensagem = "Informe o familiar desempenhará a atividade de" + 
-                ` ${atividade.toUpperCase()}:`;
+                ` ${atividade.toUpperCase()}:\n`;
             for (i = 0; i < membros.length; i++) {
                 mensagem += `${i+1} - ${membros[i]}\n`
             }
@@ -45,11 +76,11 @@ do {
             }
 
             // ATRIBUINDO ATIVIDADE A MEMBRO ESCOLHIDO
-            tarefas[membro].push(atividade.toLowerCase());
+            tarefas[membro].push(atividade.toUpperCase());
         
         
-        case '2':
-        case '3':
+        case '2':  // REMOVER TAREFA
+        case '3':  // ATUALIZAR TAREFA
         case '0':
             sair = true
             alert("Encerrando programa...");
